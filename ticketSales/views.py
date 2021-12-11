630d472ffb94ba203c1bf62b590afdc478c4b8ee
mrcod3r <mrcod3r.ir@gmail.com>
2021-12-11 09:41:23 +0330

    p8 and p9 completed

--- ticketSales/views.py
+++ ticketSales/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from ticketSales.models import concertModel
 
 # Create your views here.
 def concertListView(request):
   concerts = concertModel.objects.all()
+  context = {
+    "concertlist":concerts,
+    "concertcount":concerts.count()
+  }
 
-  text= """
-  
-  <!DOCTYPE html>
-  <html>
-    <head></head>
-    <body>
-      <h1>لیست کنسرت ها</h1>
-      <ul>
-        {}
-      </ul>
-    </body>
-  </html>
-
-  """.format('\n'.join('<li>{}</li>'.format(concert) for concert in concerts))
-  return HttpResponse(text)
-
+  return render(request,"ticketSales/concertList.html",context)
