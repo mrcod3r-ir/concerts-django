94bd233f9169894f087551d804a1b5d3fd8bb4b3
mrcod3r <mrcod3r.ir@gmail.com>
2022-01-08 11:47:40 +0330

    change project structure

--- ticketSales/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TicketsalesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ticketSales'
-    verbose_name='فروش بلیط کنسرت'
